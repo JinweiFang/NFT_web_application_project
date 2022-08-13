26c7864e33aed3f355613eec511f8b3f12ee52a4
JohnPiaPian <johnpiapian@gmail.com>
2022-08-12 21:43:30 -0500

    Create user model

---
+++ src/Model/User.java
@@ -0,0 +1,56 @@
+package Model;
+
+public class User {
+    private int id;
+    private String fName, lName, username, password;
+
+    public User(){}
+
+    public User(int id, String fName, String lName, String username, String password) {
+        this.id = id;
+        this.fName = fName;
+        this.lName = lName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
