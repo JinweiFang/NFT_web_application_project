23dedc21f508c5f0ced4be5f0a25a1d685970330
johnpiapian <johnpiapian@gmail.com>
2023-01-20 18:19:01 -0600

    Add portfolio servlet

---
+++ src/Controller/portfolioServlet.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import Domain.User;
+import Service.UserService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class portfolioServlet extends HttpServlet {
+    private UserService userService;
+    @Override
+    public void init() {this.userService = new UserService();}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Handle authorized access
+        HttpSession session = req.getSession(false);
+        User loggedUser = (session == null) ? null : (User) session.getAttribute("user");
+        if (loggedUser == null) {
+            resp.sendRedirect(req.getContextPath() + "/account/login.jsp");
+            return;
+        }
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher(req.getContextPath() + "WEB-INF/View/account/profile.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Handle authorized access
+        HttpSession session = req.getSession(false);
+        User loggedUser = (session == null) ? null : (User) session.getAttribute("user");
+        if (loggedUser == null) {
+            resp.sendRedirect(req.getContextPath() + "/account/login.jsp");
+            return;
+        }
+    }
+}
